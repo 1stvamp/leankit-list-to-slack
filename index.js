a605522a7ec43e1fa7b643025e611dc5f4d7c09e
Wes Mason <wes@1stvamp.org>
2019-03-15 11:48:07 +0000

    Support using auth token

--- index.js
+++ index.js
@@ -7,25 +7,36 @@ const moment = require('moment')
 
 const SLACK_URL = process.env.SLACK_URL
 const LEANKIT_EMAIL = process.env.LEANKIT_EMAIL
+const LEANKIT_TOKEN = process.env.LEANKIT_TOKEN
 const LEANKIT_PASSWORD = process.env.LEANKIT_PASSWORD
 const LEANKIT_ACCOUNT = process.env.LEANKIT_ACCOUNT
 const LEANKIT_BOARD_ID = process.env.LEANKIT_BOARD_ID
 const LEANKIT_LANE_NAME = process.env.LEANKIT_LANE_NAME
 
 assert(SLACK_URL)
-assert(LEANKIT_EMAIL)
-assert(LEANKIT_PASSWORD)
+assert([LEANKIT_PASSWORD, LEANKIT_TOKEN].filter(Boolean).length)
 assert(LEANKIT_ACCOUNT)
 assert(LEANKIT_BOARD_ID)
 assert(LEANKIT_LANE_NAME)
 
 const main = async () => {
   try {
-    const client = LeanKitClient({
-      account: LEANKIT_ACCOUNT,
-      email: LEANKIT_EMAIL,
-      password: LEANKIT_PASSWORD
-    })
+    let options
+    if (LEANKIT_PASSWORD) {
+      assert(LEANKIT_EMAIL)
+
+      options = {
+        account: LEANKIT_ACCOUNT,
+        email: LEANKIT_EMAIL,
+        password: LEANKIT_PASSWORD
+      }
+    } else {
+      options = {
+        account: LEANKIT_ACCOUNT,
+        token: LEANKIT_TOKEN
+      }
+    }
+    const client = LeanKitClient(options)
 
     const board = await client.board.get(LEANKIT_BOARD_ID)
     const lane = board.data.lanes.find(x => x.name.toLowerCase() === LEANKIT_LANE_NAME.toLowerCase())
@@ -42,6 +53,8 @@ const main = async () => {
     } else {
       message = 'No LeanKit cards *currently* on deck.'
     }
+    console.log(message)
+    process.exit(0)
     await fetch(SLACK_URL, {
         method: 'post',
         body:    JSON.stringify({text: message}),
